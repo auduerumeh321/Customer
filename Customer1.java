ac789080f3037caa4d74a4aa887b220bc762457d
auduerumeh321 <erumehjonathan@yahoo.com>
2020-08-24 04:59:21 +0200

    add a variable

--- Customer1.java
+++ Customer1.java
@@ -2,6 +2,8 @@ class Customer1{
 String name;
 int age;
 long phoneNumber;
+Sting tax;
+
 
 Customer1(){}
 
@@ -9,6 +11,7 @@ Customer1(String name,int age,long phoneNumber){
 this.name=name;
 this.age=age;
 this.phoneNumber=phoneNumber;
+this .tax=tax;
 
 }
 }
